d270de66588be3ca948f12f19f08d9ed0914fae5
youssef sbai <155957681+wizard5919@users.noreply.github.com>
2025-08-08 11:42:42 +0100

    Update 1_Home.py

--- pages/1_Home.py
+++ pages/1_Home.py
@@ -1,4 +1,4 @@
-cat > pages/1_Home.py << 'EOL'
+
 import streamlit as st
 from PIL import Image
 import os
@@ -84,4 +84,5 @@ with footer_cols[1]:
     st.markdown("[GitHub](https://github.com/wizard5919) • [LinkedIn](https://linkedin.com)")
 with footer_cols[2]:
     st.markdown("**Data Sources:** FRED • Yahoo Finance • OANDA")
-EOL
+
+
