144b6c19ff65697a72925fb396dd8df9adbb1eca
youssef sbai <155957681+wizard5919@users.noreply.github.com>
2025-08-14 13:08:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from pathlib import Path
-from pages import _1_Home
+from pages import home, projects, dashboard, insights, contact, options_analyzer, sector_classifier, macro_dashboard
+
 
 
 # Optional: set page config globally
