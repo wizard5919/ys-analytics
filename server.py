5bfbfff15dd4eba644c038622dd3963fe1076094
youssef sbai <155957681+wizard5919@users.noreply.github.com>
2025-08-14 12:10:24 +0100

    Update server.py

--- server.py
+++ server.py
@@ -2,18 +2,13 @@ from streamlit.web import bootstrap
 import os
 
 def main():
-    # Get port from Vercel environment variable
     port = int(os.environ.get("PORT", 8501))
-    
-    # Configure Streamlit
     bootstrap.run(
-        "app.py",
+        "Home.py",
         command_line=None,
         args=[
             "--server.port", str(port),
-            "--server.headless", "true",
-            "--server.enableCORS", "false",
-            "--server.enableXsrfProtection", "false"
+            "--server.headless", "true"
         ],
         flag_options={}
     )
